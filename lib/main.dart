5b770a389fe2539992f192a6bbb97137c0acf726
Moawiah999 <muawiyah454@gmail.com>
2022-09-03 19:35:35 +0300

    country

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,106 +11,134 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return Home();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  List<Color> mycolor = [Colors.orange, Colors.blueGrey, Colors.brown];
+  List<Color> mydeflultcolor = [Colors.blue, Colors.white12];
+  bool myCheckbox = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  bool myCheckboxone = false;
+  bool myCheckboxonetwo = false;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: ListView(
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              "enter your cantry",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: mycolor),
+              ),
+              child: CheckboxListTile(
+                secondary: Image(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        "https://th.bing.com/th/id/OIP.IshStqXynXGh5_3GFOoICQHaFj?w=216&h=180&c=7&r=0&o=5&pid=1.7")),
+                checkColor: Colors.redAccent,
+                subtitle: Text("amman",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 8, 2, 95), fontSize: 25)),
+                title: Text(
+                  "jorden",
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
+                value: myCheckbox,
+                onChanged: (value) {
+                  setState(() {
+                    myCheckbox = value!;
+                  });
+                },
+              ),
+            ),
+            Divider(
+              height: 20,
+              thickness: 15,
+              color: Colors.cyanAccent,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: mycolor),
+              ),
+              child: CheckboxListTile(
+                secondary: Image(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        "https://th.bing.com/th/id/OIP.5gaTuN7P2p9IqAKcl2pgrgHaE8?w=256&h=180&c=7&r=0&o=5&pid=1.7")),
+                checkColor: Colors.redAccent,
+                subtitle: Text("damascus",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 8, 2, 95), fontSize: 25)),
+                title: Text(
+                  "syria",
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
+                value: myCheckboxone,
+                onChanged: (value) {
+                  setState(() {
+                    myCheckboxone = value!;
+                  });
+                },
+              ),
+            ),
+            Divider(
+              height: 20,
+              thickness: 15,
+              color: Color.fromARGB(255, 0, 66, 66),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: mycolor),
+              ),
+              child: CheckboxListTile(
+                secondary: Image(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        "https://th.bing.com/th/id/OIP.kEBN7KppqYhigMgZj9mOUgHaEw?w=253&h=180&c=7&r=0&o=5&pid=1.7")),
+                checkColor: Colors.redAccent,
+                subtitle: Text("Riyadh",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 8, 2, 95), fontSize: 25)),
+                title: Text(
+                  "saudi arabia",
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
+                value: myCheckboxonetwo,
+                onChanged: (value) {
+                  setState(() {
+                    myCheckboxonetwo = value!;
+                  });
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
